9314ee389bfa247b4970b8633bd714c917bbd587
winnd <AS40879@yeah.net>
2018-01-18 15:40:10 +0800

    Merge branch 'master' of https://github.com/winnd/bishe-vue-new
    # Conflicts:
    #	.travis.yml

--- config/index.js
+++ config/index.js
@@ -10,8 +10,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static', // r_原来是static
-    assetsPublicPath: './',
-    productionSourceMap: false,    // r_原来是true
+    assetsPublicPath: './bishe-vue-new/',
+    productionSourceMap: true,    // r_原来是true
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
