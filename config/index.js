c9627c1a6f4280d64a4fc4ae4bb24f53da23ba6c
winnd <AS40879@yeah.net>
2018-01-18 22:39:52 +0800

    静态资源位置测试 index.js -v3

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: './bishe-vue-new/static', // r_原来是static
-    assetsPublicPath: './', // r_原来是./
+    assetsPublicPath: './bishe-vue-new', // r_原来是./
     productionSourceMap: false,    // r_原来是true
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
