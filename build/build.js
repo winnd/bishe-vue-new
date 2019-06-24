1c23f08d4fc1fb7b40e7f5be9449ba34253963ae
winnd <AS40879@yeah.net>
2019-06-25 02:26:25 +0800

    预览图

--- build/build.js
+++ build/build.js
@@ -1,12 +1,12 @@
 'use strict'
 require('./check-versions')()
 
-process.env.NODE_ENV = 'production'
+process.env.NODE_ENV = 'production'                   // process对象是一个global（全局变量），提供有关信息，控制当前Node.js进程。
 
-const ora = require('ora')
-const rm = require('rimraf')
-const path = require('path')
-const chalk = require('chalk')
+const ora = require('ora')                            // 主要用来实现node.js命令行环境的loading效果，和显示各种状态的图标等。
+const rm = require('rimraf')                          // 实现node.js环境的UNIX命令rm -rf                                           // rimraf(f, [opts], callback)
+const path = require('path')                          // path模块提供了一些工具函数，用于处理文件与目录的路径。这是node.js一个自带的模块。 会根据Node.js应用程序运行的操作系统的不同而变化
+const chalk = require('chalk')                        // 命令行输出各种样式的字符串。                                                // chalk.<style>[.<style>...](string, [string...]) chalk.red.bold.underline('Hello', 'world')
 const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
